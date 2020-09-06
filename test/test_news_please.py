6217e54277a8c254ca848b1ceedb721f93928541
onomatopeia <agata.migalska@gmail.com>
2020-09-06 22:59:26 +0200

    Initial investigation on Named Entity Recognition in Polish

--- test/test_news_please.py
+++ test/test_news_please.py
@@ -8,3 +8,19 @@ class TestNewsPlease(TestCase):
         article = downloader.fetch(url)
         print(article.title)
         print(article.maintext)
+
+    def test2(self):
+        # Gazeta wyborcza required subscription and blocks its content for webcrawling
+        url = r'https://wroclaw.wyborcza.pl/wroclaw/7,35771,26231183,ognisko-koronawirusa-na-pogotowiu-jest-wiecej-zakazonych.html'
+        article = downloader.fetch(url)
+        downloader.describe_article(article)
+
+    def test3(self):
+        url = r'https://radio.lublin.pl/2020/08/4-nowe-ogniska-koronawirusa-wojewodztwie-lubelskim/'
+        article = downloader.fetch(url)
+        downloader.describe_article(article)
+
+    def test4(self):
+        url = r'https://m.sadeczanin.info/wiadomosci/nowy-sacz-ognisko-koronawirusa-w-dps-sa-juz-wyniki-badan'
+        article = downloader.fetch(url)
+        downloader.describe_article(article)
--- webscrapping/downloader.py
+++ webscrapping/downloader.py
@@ -20,6 +20,24 @@ def fetch(link):
     return NewsPlease.from_url(link)
 
 
+def describe_article(article):
+    print('Authors', article.authors)
+    print('Date download', article.date_download)
+    print('Date modify', article.date_modify)
+    print('Date publish', article.date_publish)
+    print('Description', article.description)
+    print('File name', article.filename)
+    print('Image URL', article.image_url)
+    print('Language', article.language)
+    print('Local path', article.localpath)
+    print('Title', article.title)
+    print('Title page', article.title_page)
+    print('Title RSS', article.title_rss)
+    print('Source domain', article.source_domain)
+    print('Main text', article.maintext)
+    print('URL', article.url)
+
+
 if __name__ == '__main__':
     start = 50
     stop = start + 50
