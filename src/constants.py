b338ea4a4fa04da3a3632b7324af8c3c7dff0970
onomatopeia <agata.migalska@gmail.com>
2020-10-13 20:35:30 +0200

    preparation for classification

--- src/constants.py
+++ src/constants.py
@@ -2,3 +2,4 @@ from pathlib import Path
 PROJECT_DIR = Path(__file__).parents[1].resolve()
 DATA_DIR = PROJECT_DIR / 'data'
 DATABASE = DATA_DIR / 'search_results.db'
+PROCESSED_DATA_DIR = DATA_DIR / 'processed'
--- src/data/prepare_dataset.py
+++ src/data/prepare_dataset.py
@@ -1,13 +1,15 @@
 import logging
 from typing import List
 from datetime import datetime
-
+from pathlib import Path
+import csv
 from newspaper.article import ArticleDownloadState
 
 from src.data.database import get_links, get_session, get_url_attributes
 from src.data.entities import URLAttributes, URL
 from src.ner import processor
 from src.webscrapping.downloader import fetch_with_newspaper
+from src.constants import PROCESSED_DATA_DIR
 
 
 def load_articles(links: List[URL]) -> None:
@@ -30,18 +32,37 @@ def load_articles(links: List[URL]) -> None:
         session.commit()
 
 
-def retrieve_named_entities(articles):
+def retrieve_named_entities(output_file: Path, articles: List[URLAttributes], ner_type: str ='CARDINAL') -> int:
+    # article processor
     proc = processor.ArticleProcessor()
-    for article in articles:
-        information = proc.run([article.title, article.meta_description, article.text])
-        print(information)
-        break
+    # output output csv file
+    with output_file.open('w', newline='', encoding='utf-8') as csvfile:
+        # create a csv writer object
+        csv_writer = csv.writer(csvfile)
+        # count the number of successfully processed articles
+        successful = 0
+        # for each article
+        for article in articles:
+            # get all relevant sentences
+            sentences = [article.title, article.description, article.maintext]
+            # process for classification i.e. for each ner_type get 16 tokens before and 16 after
+            try:
+                for phrase in proc.prepare_for_classification(ner_type, sentences):
+                    # write that to a csv
+                    csv_writer.writerow(phrase)
+                successful += 1
+            except RuntimeError as e:
+                print(e)
+                print(f'Failure in : {article.url_id}, skipping')
+        return successful
 
 
 def main():
     load_articles(get_links())
-    retrieve_named_entities(get_url_attributes())
 
 
 if __name__ == '__main__':
-    retrieve_named_entities(get_url_attributes())
+    output_file = PROCESSED_DATA_DIR / 'cardinal_classification.csv'
+    all_articles = get_url_attributes()
+    successful = retrieve_named_entities(output_file, all_articles)
+    print(f'successfully retrived classification clauses from {successful} artilces out of {len(all_articles)}')
